91761136ec92722f5daeb8368021452a63b23926
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2021-01-03 14:50:55 -0500

    inheritance

--- jlox/com/zenghao/lox/Interpreter.java
+++ jlox/com/zenghao/lox/Interpreter.java
@@ -87,7 +87,19 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    Object superclass = null;
+    if (stmt.superclass != null) {
+      superclass = evaluate(stmt.superclass);
+      if (!(superclass instanceof LoxClass)) {
+        throw new RuntimeError(stmt.superclass.name, "Superclass must be a class.");
+      }
+    }
     environment.define(stmt.name.lexeme, null);
+
+    if (stmt.superclass != null) {
+      environment = new Environment();
+      environment.define("super", superclass);
+    }
 
     Map<String, LoxFunction> methods = new HashMap<>();
     for (Stmt.Function method : stmt.methods) {
@@ -95,7 +107,10 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
       methods.put(method.name.lexeme, function);
     }
 
-    LoxClass _class = new LoxClass(stmt.name.lexeme, methods);
+    LoxClass _class = new LoxClass(stmt.name.lexeme, (LoxClass) superclass, methods);
+    if (superclass != null) {
+      environment = environment.enclosing;
+    }
     environment.assign(stmt.name, _class);
     return null;
   }
@@ -204,6 +219,20 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     Object value = evaluate(expr.value);
     ((LoxInstance) object).set(expr.name, value);
     return value;
+  }
+
+  @Override
+  public Object visitSuperExpr(Expr.Super expr) {
+    int distance = locals.get(expr);
+    var superclass = (LoxClass) environment.getAt(distance, "super");
+
+    var object = (LoxInstance) environment.getAt(distance - 1, "this");
+
+    var method = superclass.findMethod(expr.method.lexeme);
+    if (method == null) {
+      throw new RuntimeError(expr.method, "Undefined property '" + expr.method.lexeme + "'.");
+    }
+    return method.bind(object);
   }
 
   @Override
--- jlox/com/zenghao/lox/Parser.java
+++ jlox/com/zenghao/lox/Parser.java
@@ -53,6 +53,13 @@ public class Parser {
 
   private Stmt classDeclaration() {
     Token name = consume(TokenType.IDENTIFIER, "Expect class name.");
+
+    Expr.Variable superclass = null;
+    if (match(TokenType.LESS)) {
+      consume(TokenType.IDENTIFIER, "Expect superclass name.");
+      superclass = new Expr.Variable(previous());
+    }
+
     consume(TokenType.LEFT_BRACE, "Expect '{' before class body.");
 
     List<Stmt.Function> methods = new ArrayList<>();
@@ -61,7 +68,7 @@ public class Parser {
     }
 
     consume(TokenType.RIGHT_BRACE, "Expect '}' after class body.");
-    return new Stmt.Class(name, methods);
+    return new Stmt.Class(name, superclass, methods);
   }
 
   private Stmt varDeclaration() {
@@ -396,6 +403,13 @@ public class Parser {
 
     if (match(TokenType.NUMBER, TokenType.STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(TokenType.SUPER)) {
+      Token keyword = previous();
+      consume(TokenType.DOT, "Expect '.' after 'super'.");
+      Token method = consume(TokenType.IDENTIFIER, "Expect superclass method name.");
+      return new Expr.Super(keyword, method);
     }
 
     if (match(TokenType.THIS)) {
